5b8b9dbbd70acc2220388ab13d85eb067adf62ed
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-04-14 11:43:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ numTest = int(input("Digite um número: "))
 
 # imprimir os n primeiros números primos, um por linha.
 def primoTeste(numero):
+    if numero == 1:
+        return False
+    
     e_primo = True
     for i in range(2, numero):
-        if numero % i == 0:
+        if numero % i == 0 or numero == 1:
             e_primo = False
             break
     return e_primo
-    
-n = 2
-while n < numTest :
+
+n = 1
+while n <= numTest:
     if primoTeste(n) :
         print(n)
     n += 1
