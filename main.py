4dc3c9ab3c9b9299825253ac4633697d52669b0c
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-04-14 13:35:20 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-# O programa deve receber um número inteiro n digitado pelo usuário
-numTest = int(input("Digite um número: ")) 
+numTest = int(input("Digite um número: ")) 
   
- # imprimir os n primeiros números primos, um por linha. 
- def primoTeste(numero): 
-     if numero == 1: 
-         return False 
-      
-     e_primo = True 
-     for i in range(2, numero): 
-         if numero % i == 0 or numero == 1: 
-             e_primo = False 
-             break 
-     return e_primo 
+ # imprimir os n primeiros números primos, um por linha. 
+def primoTeste(numero): 
+  e_primo = True 
   
- n = 0
- x = 1
- while n <= numTest: 
-     if primoTeste(x) : 
-         print(x) 
-     n -= 1
-     x += 1
- 
+  if numero == 1: 
+    return False 
+    
+  for i in range(2, numero): 
+    if numero % i == 0 or numero == 1: 
+      return False
+      break 
+  return True 
+  
+n = numTest
+x = 2
+while n != 0: 
+  if primoTeste(x) : 
+    print(x)
+    n -= 1
+  x += 1
